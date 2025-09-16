d60c3563ab0c6fdce51099861b6eca919a15b011
Yahia Salman <yahia.salman.04@gmail.com>
2025-09-16 11:15:50 -0400

    adding removals

--- app.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
-@app.post("/")
-def hello_world():
-    return {"text": "Hello World"}
--- start.sh
+++
@@ -1,1 +0,0 @@
-uvicorn app:app --reload --port 8000 --host 0.0.0.0
